6ae07460393db7dec69ace67d33fdcaf23ebbb5b
Noah Levenson <noahlevenson@gmail.com>
2020-11-03 19:31:16 -0500

    add tip and comment to hbuy_order

--- js/src/hbuy/hbuy_order.js
+++ js/src/hbuy/hbuy_order.js
@@ -9,9 +9,6 @@
 
 "use strict";
 
-const { Hbuy_item_ref } = require("./hbuy_item_ref.js");
-const { Hbuy_item_misc } = require("./hbuy_item_misc.js");
-
 class Hbuy_order {
 	static TYPE = {
 		DELIVERY: 0,
@@ -21,28 +18,24 @@ class Hbuy_order {
 	type;
 	item_refs;
 	misc_items;
+	tip;
+	comment;
 
-	constructor({type = null} = {}) {
+	constructor({type = null, item_refs = [], misc_items = [], tip = null, comment = null} = {}) {
 		this.type = type;
-		this.item_refs = [];
-		this.misc_items = [];
+		this.item_refs = item_refs;
+		this.misc_items = misc_items;
+		this.tip = tip;
+		this.comment = comment;
 	}
 
 	// Add a standard item to the order
 	add(item_ref) {
-		if (!(item_ref instanceof Hbuy_item_ref)) {
-			throw new TypeError("Argument 'item_ref' must be an Hbuy_item_ref");
-		}
-
 		this.item_refs.push(item_ref);
 	}
 
 	// Add a misc item to the order
 	add_misc(misc_item) {
-		if (!(misc_item instanceof Hbuy_item_misc)) {
-			throw new TypeError("Argument 'misc_item' must be an Hbuy_item_misc");
-		}
-
 		this.misc_items.push(misc_item);
 	}
 
