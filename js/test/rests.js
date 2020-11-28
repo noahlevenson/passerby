4d88576ee3a9c30a012cc8b435ec6cb95d39ce3c
Noah Levenson <noahlevenson@gmail.com>
2020-11-28 11:35:58 -0500

    typo in demo script

--- js/test/rests.js
+++ js/test/rests.js
@@ -59,7 +59,7 @@ const { Rocnramen_menu } = require("./rocnramen_menu.js");
     await network.hpht.insert(alvin.linearize(), new Happ_bboard({cred: new Hid_pub({pubkey: "alvinandfriends", name: "Alvin & Friends", address: "14 Memorial Highway New Rochelle NY 10801", phone: "(914) 654-6549"}), form: Alvin_friends_dinner_menu.freeze()}));
 
     const rocnramen = new Hgeo_coord({lat: 40.9111487, long: -73.7832296});
-    await network.hpht.insert(rocnramen.linearize(), new Happ_bboard({cred: new Hid_pub({pubkey: "rocnramen", name: "Roc N Ramen", address: "19 Anderson St. NY 10801", phone: "(914) 365-2267"}), form: Rocnramen_menu.freeze()}));
+    await network.hpht.insert(rocnramen.linearize(), new Happ_bboard({cred: new Hid_pub({pubkey: "rocnramen", name: "Roc N Ramen", address: "19 Anderson St. New Rochelle NY 10801", phone: "(914) 365-2267"}), form: Rocnramen_menu.freeze()}));
 
     console.log("Done!")
 })();
