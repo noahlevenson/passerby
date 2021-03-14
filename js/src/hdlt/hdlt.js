afdcbefe6421cb4cb6083f69fbc37447b0a88f16
Noah Levenson <noahlevenson@gmail.com>
2021-03-13 19:32:39 -0500

    whoops

--- js/src/hdlt/hdlt.js
+++ js/src/hdlt/hdlt.js
@@ -180,7 +180,7 @@ class Hdlt {
 
 					// Add the new block, rebuild the store index, broadcast it, and get to work on the next block
 					const new_node = new Hntree_node({data: new_block, parent: pred_block_node})
-					parent.add_child(new_node);
+					pred_block_node.add_child(new_node);
 					this.store.build_dict();
 					Hlog.log(`[HDLT] (${this.net.app_id}) Made new block ${block_hash}, ${this.store.size()} blocks total`);
 					this.broadcast(this.block_req, {hdlt_block: new_block});
