f5a8b89669dd49afd3e3c61a1e4d32efd9c504d8
Noah Levenson <noahlevenson@gmail.com>
2021-04-21 10:17:09 -0400

    add img_cred_base64 field to fapp_bboard

--- js/src/fapp/fapp_bboard.js
+++ js/src/fapp/fapp_bboard.js
@@ -1,8 +1,8 @@
 /** 
 * FAPP_BBOARD
-* FAPP_BBOARD (billboard) is to Free Food what torrents 
-* are to BitTorrent: they are publicly shareable data 
-* entities which describe a network resource (food)
+* FAPP_BBOARD is to Free Food what a torrent is
+* to BitTorrent: it's a publicly shareable data 
+* structure which describes a network resource (food)
 * A restaurant publishes a Fapp_bboard to make itself 
 * discoverable to diners
 */ 
@@ -11,16 +11,23 @@
 
 class Fapp_bboard {
 	cred;
+	img_cred_base64;
 	form;
-
-	constructor({cred = null, form = null}  = {}) {
+	
+	constructor({cred = null, img_cred_base64 = null, form = null}  = {}) {
 		if (!Array.isArray(form.data)) {
 			throw new Error("form.data doesn't seem to be an array - are you sure you're publishing a frozen form?");
 		}
 
-		this.cred = cred; // Generalization of a signed cryptographic certificate
-		this.form = form; // Generalization of a food menu
+		// Generalization of a identity credential, most likely a Fid_pub object
+		this.cred = cred; 
+		// A base64 encoded image correlating your real world identity with the one named in cred, see Fid.get_symbol_indices
+		this.img_cred_base64 = img_cred_base64; 
+		// Generalization of a food menu
+		this.form = form;
 	}
+
+	// TODO: write a static method to safely validate size (in bytes) and dimensions of img_cred_base64
 }
 
 module.exports.Fapp_bboard = Fapp_bboard;
