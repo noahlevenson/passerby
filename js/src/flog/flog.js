2d0b5bc510b6cdbcf1609dada3a5d05e6051308d
Noah Levenson <noahlevenson@gmail.com>
2021-04-29 11:58:52 -0400

    cleanup: flog, fnative

--- js/src/flog/flog.js
+++ js/src/flog/flog.js
@@ -10,28 +10,28 @@
 "use strict";
 
 class Flog {
-	static LOGF = console.log;
-	static ERRF = console.err;
+  static LOGF = console.log;
+  static ERRF = console.err;
 
-	static set_log(f) {
-		if (typeof f !== "function") {
-			throw new TypeError("Argument f must be a function");
-		}
+  static set_log(f) {
+    if (typeof f !== "function") {
+      throw new TypeError("Argument f must be a function");
+    }
 
-		Flog.LOGF = f;
-	}
+    Flog.LOGF = f;
+  }
 
-	static set_err(f) {
-		if (typeof f !== "function") {
-			throw new TypeError("Argument f must be a function");
-		}
+  static set_err(f) {
+    if (typeof f !== "function") {
+      throw new TypeError("Argument f must be a function");
+    }
 
-		Flog.ERRF = f;
-	}
+    Flog.ERRF = f;
+  }
 
-	static log(msg, ln = false, tn = false) {
-		Flog.LOGF(`${ln ? "\n" : ""} ${new Date().toLocaleString()} ${msg}${tn ? "\n" : ""}`);
-	}
+  static log(msg, ln = false, tn = false) {
+    Flog.LOGF(`${ln ? "\n" : ""} ${new Date().toLocaleString()} ${msg}${tn ? "\n" : ""}`);
+  }
 }
 
 module.exports.Flog = Flog;
