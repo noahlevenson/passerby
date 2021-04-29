8fb89e53b31f0b3783e28c4662aae5c8539b4c64
Noah Levenson <noahlevenson@gmail.com>
2021-04-29 15:08:48 -0400

    cleanup: futil

--- js/src/futil/futil.js
+++ js/src/futil/futil.js
@@ -9,266 +9,260 @@
 
 "use strict";
 
-const net = require("net"); // TODO: We currently have no browser implementation for 'net'
 const { Fapp_env } = require("../fapp/fapp_env.js");
+const { Fbigint } = Fapp_env.ENV === Fapp_env.ENV_TYPE.REACT_NATIVE ? 
+  require("../ftypes/fbigint/fbigint_rn.js") : require("../ftypes/fbigint/fbigint_node.js");
+// TODO: We currently have no React Native or browser implementation for 'net'
+const net = require("net");
 const crypto =  require("crypto");
-const { Fbigint } = Fapp_env.ENV === Fapp_env.ENV_TYPE.REACT_NATIVE ? require("../ftypes/fbigint/fbigint_rn.js") : require("../ftypes/fbigint/fbigint_node.js");
 
 class Futil {
-	// JSON serializer for javascript's Map() type -- this is set on the Map prototype at the FAPP layer as above
-	// We just turn maps into arrays of key, val pairs in our over-the-wire format
-	static _map_to_json() {
-		return JSON.stringify(Array.from(this.entries()));
-	}
+  // JSON serializer for JS Map type
+  static _map_to_json() {
+    return JSON.stringify(Array.from(this.entries()));
+  }
 
-	// Since maps come over the wire as arrays, there's no way to tell that they're supposed to be maps - so we can't write
-	// a reviver function for them...
-	// Instead, we implement a Map.from_JSON() function (curently set on the prorotype at the FAPP layer)
-	// TODO: We needlessly (?) re-stringify and re-parse the contents of the Map because the JSON.parse reviver function
-	// can't seem to deal with certain kinds of nested objects?
-	static _map_from_json(json) {
-		const arr = JSON.parse(json);
-		const deeply_parsed = arr.map((elem) => {
-			return JSON.parse(JSON.stringify(elem), Fbigint._json_revive);
-		});
-
-		return new Map(deeply_parsed);
-	}
-
-	// Returns a hex string
-	static _sha1(data) {
-		const hash = crypto.createHash("SHA1"); // TODO: this encoding string is host dependent, 'openssl list -digest-algorithms'
-		hash.update(data);
-		return hash.digest("hex");
-	}
-
-	// Returns a hex string
-	static _sha256(data) {
-		const hash = crypto.createHash("SHA256");
-		hash.update(data);
-		return hash.digest("hex");
-	}
+  // JSON deserializer for JS Map type
+  static _map_from_json(json) {
+    const arr = JSON.parse(json);
     
-    static _is_power2(n) {
-        return (n & (n - 1)) === 0;
+    const deeply_parsed = arr.map((elem) => {
+      return JSON.parse(JSON.stringify(elem), Fbigint._json_revive);
+    });
+
+    return new Map(deeply_parsed);
+  }
+
+  // Returns a hex string
+  static _sha1(data) {
+    const hash = crypto.createHash("SHA1");
+    hash.update(data);
+    return hash.digest("hex");
+  }
+
+  // Returns a hex string
+  static _sha256(data) {
+    const hash = crypto.createHash("SHA256");
+    hash.update(data);
+    return hash.digest("hex");
+  }
+    
+  static _is_power2(n) {
+    return (n & (n - 1)) === 0;
+  }
+
+  static _is_hex_str(str) {
+    const reg = /^[A-Fa-f0-9]+$/;
+    return reg.test(str);
+  }
+
+  // Get the integral part of the log2 of a Fbigint
+  static _log2(n) {
+    let x = new Fbigint(n);
+    
+    if (x.equals(new Fbigint(0))) {
+      return 0;
+    }
+    
+    let zero = new Fbigint(0);
+    let y = new Fbigint(0x01);
+    let i = 0;
+
+    while (x.greater(zero)) {
+      x = x.shift_right(y);
+      i += 1
     }
 
-    static _is_hex_str(str) {
-    	const reg = /^[A-Fa-f0-9]+$/;
-    	return reg.test(str);
+    return i - 1;
+  }
+
+  // Sort numbers in an array by absolute difference
+  // TODO: this function has a dumb name
+  static _sort_by_distance_from(arr, n) {
+    return arr.sort((a, b) => {
+      return Math.abs(a - n) - Math.abs(b - n);
+    });
+  }
+
+  // Normalize positive float f to an integer of bit depth b, where fmax is the largest poss value of f
+  static _float_to_normalized_int(f, fmax, b) {
+    // TODO: Validate inputs and watch out for overflow
+    const max = (Math.pow(2, b) - 1) / fmax;
+    return Math.floor(f * max);
+  }
+
+  // Compute 2D Morton order linearization for positive values x and y, where b is the bit depth of 
+  // each dimension (x becomes odd bits, y becomes even bits)
+  static _z_linearize_2d(x, y, b) {
+    // TODO: Validate inputs
+    let xx = new Fbigint(x);
+    let yy = new Fbigint(y);
+
+    let l = new Fbigint(0);
+    let mask = new Fbigint(0x01);
+
+    for (let i = 0; i < b; i += 1) {
+      l = l.or((xx.and(mask)).shift_left(new Fbigint(i)));
+      l = l.or((yy.and(mask)).shift_left(new Fbigint(i + 1)));
+      mask = mask.shift_left(new Fbigint(0x01));
     }
 
-	// Get the integral part of the log2 of a Fbigint
-	// Works on Number types too, but pointlessly slow
-	static _log2(n) {
-		let x = new Fbigint(n);
-		
-		if (x.equals(new Fbigint(0))) {
-			return 0;
-		}
-		
-		let zero = new Fbigint(0);
-		let y = new Fbigint(0x01);
-		let i = 0;
+    return l;
+  }
 
-		while (x.greater(zero)) {
-			x = x.shift_right(y);
-			i += 1
-		}
+  // Invert _z_linearize_2d
+  static _z_delinearize_2d(key, b) {
+    let x = "";
+    let y = "";
 
-		return i - 1;
-	}
+    [...key.to_bin_str(b)].forEach((char, i) => {
+      if (i % 2 === 0) {
+        y = `${y}${char}`;
+      } else {
+        x = `${x}${char}`;
+      }
+    });
 
-	// Sort elements in array arr by their euclidean distance from number n
-	// It's meant to operate on Number types
-	static _sort_by_distance_from(arr, n) {
-		return arr.sort((a, b) => {
-			return Math.abs(a - n) - Math.abs(b - n);
-		});
-	}
+    return {x: Fbigint.from_base2_str(x), y: Fbigint.from_base2_str(y)};
+  }
 
-	// Normalize positive float f to an integer of bit depth b, where fmax is the largest possible value of f
-	// Currently returns a Number, but we may want to instead roll our own Buffer-based binary format
-	// The reason we don't just return a BigInt is because we can't multiply a float by a BigInt
-	// TODO: Currently appears safe for the range we need for lat/long, but it's undefined and may overflow for other use cases!
-	static _float_to_normalized_int(f, fmax, b) {
-		// TODO: Validate inputs and make sure we won't overflow!
-		const max = (Math.pow(2, b) - 1) / fmax;
-		return Math.floor(f * max);
-	}
+  // Compute the longest common prefix of an array of strings
+  // if len = true, return the length of the lcp instead of the lcp itself
+  // TODO: there's a more alpha way to do this with binary search
+  static _get_lcp(strings = [], len = false) {
+    if (!Array.isArray(strings)) {
+      throw new TypeError("Argument 'strings' must be array");
+    }
 
-	// Get 2D Z-curve linearization for positive values x and y, where b is the bit depth of each dimension
-	// Currently takes Number types and returns a BigInt, but we may want to roll our own Buffer-based binary format
-	// x becomes odd bits, y becomes even bits
-	static _z_linearize_2d(x, y, b) {
-		// TODO: Validate inputs
-		let xx = new Fbigint(x);
-		let yy = new Fbigint(y);
+    const all_strings = strings.every((str) => {
+      return typeof str == "string";
+    });
 
-		let l = new Fbigint(0);
-		let mask = new Fbigint(0x01);
+    if (!all_strings) {
+      throw new TypeError("All elements of argument 'strings' must be strings");
+    }
 
-		for (let i = 0; i < b; i += 1) {
-			l = l.or((xx.and(mask)).shift_left(new Fbigint(i)));
-			l = l.or((yy.and(mask)).shift_left(new Fbigint(i + 1)));
-			mask = mask.shift_left(new Fbigint(0x01));
-		}
+    const lengths = strings.map((str) => {
+      return str.length;
+    });
 
-		return l;
-	}
+    const shortest = Math.min(...lengths);
+    let i = 0;
+    
+      while (i < shortest) {
+        const match = strings.every((str) => {
+          return str[i] === strings[0][i];
+        });
 
-	// Reverse _z_linearize_2d
-	static _z_delinearize_2d(key, b) {
-		let x = "";
-		let y = "";
+        if (!match) {
+          break;
+        }
 
-		[...key.to_bin_str(b)].forEach((char, i) => {
-			if (i % 2 === 0) {
-				y = `${y}${char}`;
-			} else {
-				x = `${x}${char}`
-			}
-		});
+        i += 1;
+      }
 
-		return {x: Fbigint.from_base2_str(x), y: Fbigint.from_base2_str(y)};
-	}
+    if (len) {
+      return i;
+    }
 
-	// Return the longest common prefix of an array of strings
-	// If len is true, function returns the LENGTH of the lcp instead of the lcp itself
-	// TODO: this is a slow linear search solution, there's a better way with binary search
-	static _get_lcp(strings = [], len = false) {
-		if (!Array.isArray(strings)) {
-			throw new TypeError("Argument 'strings' must be array");
-		}
+    return strings[0].substring(0, i);
+  }
 
-		const all_strings = strings.every((str) => {
-			return typeof str == "string";
-		});
+  // TODO: Validation
+  // Network byte order (big endian)
+  static _int2Buf16(int) {
+    const buf = Buffer.alloc(2);
 
-		if (!all_strings) {
-			throw new TypeError("All elements of argument 'strings' must be strings");
-		}
+    buf[0] = 0xFF & (int >>> Fapp_env.SYS_BYTE_WIDTH);
+    buf[1] = 0xFF & int;
 
-		const lengths = strings.map((str) => {
-			return str.length;
-		});
+    return buf;
+  }
 
-		const shortest = Math.min(...lengths);
-		let i = 0;
-		
-			while (i < shortest) {
-				const match = strings.every((str) => {
-					return str[i] === strings[0][i];
-				});
+  // Little endian addressing
+  static _getBit(buffer, idx, off) {
+    let mask = Buffer.alloc(1);
 
-				if (!match) {
-					break;
-				}
+    mask[0] = 0x01;
+    mask[0] <<= off;
 
-				i += 1;
-			}
+    return (buffer[idx] & mask[0]) !== 0 ? 1 : 0;
+  }
 
-		if (len) {
-			return i;
-		}
+  static _compareBuf(a, b) {
+    if (!Buffer.isBuffer(a) || !Buffer.isBuffer(b)) {
+      return false
+    }
 
-		return strings[0].substring(0, i);
-	}
+    if (a.length !== b.length) {
+      return false;
+    }
 
-	// TODO: Validation
-	// Network byte order (big endian)
-	static _int2Buf16(int) {
-		const buf = Buffer.alloc(2);
+    for (let i = 0; i < a.length; i += 1) {
+      if (a[i] != b[i]) {
+        return false;
+      }
+    } 
 
-		buf[0] = 0xFF & (int >>> Fapp_env.SYS_BYTE_WIDTH);
-		buf[1] = 0xFF & int;
+    return true;
+  }
 
-		return buf;
-	}
+  // TODO: Validation
+  static _ipv4Str2Buf32(str) {
+    return Buffer.from(str.split(".").map((n) => { 
+      return parseInt(n); 
+    }));
+  }
 
-	// Little endian addressing
-	static _getBit(buffer, idx, off) {
-		let mask = Buffer.alloc(1);
+  // TODO: validation
+  static _buf32_2_ipv4Str(buf) {
+    return `${buf[0]}.${buf[1]}.${buf[2]}.${buf[3]}`;
+  }
 
-		mask[0] = 0x01;
-		mask[0] <<= off;
+  // TODO: Validation
+  static _ipv6Str2Buf128(str) {   
+    const arr = str.split(":");
+    const len = arr.length - 1;
 
-		return (buffer[idx] & mask[0]) !== 0 ? 1 : 0;
-	}
+    // It's an ipv4 mapped ipv6 address
+    if (net.isIPv4(arr[len]) && arr[len - 1].toUpperCase() === "FFFF") {
+      arr[len] = arr[len].split(".").map((n) => {
+        return parseInt(n).toString(16).padStart(2, "0");
+      }).join("");
+    }
 
-	static _compareBuf(a, b) {
-		if (!Buffer.isBuffer(a) || !Buffer.isBuffer(b)) {
-			return false
-		}
+    const hs = arr.join("").padStart(16, "0");
+    const buf = Buffer.alloc(16);
 
-		if (a.length !== b.length) {
-			return false;
-		}
+    let i = hs.length - 2;
+    let j = buf.length - 1;
 
-		for (let i = 0; i < a.length; i += 1) {
-			if (a[i] != b[i]) {
-				return false;
-			}
-		}	
+    while (i >= 0) {
+      buf[j] = parseInt(hs.substring(i, i + 2), 16);
+      i -= 2;
+      j -= 1;
+    }
 
-		return true;
-	}
+    return buf;
+  }
 
-	// TODO: Validation
-	static _ipv4Str2Buf32(str) {
-		return Buffer.from(str.split(".").map((n) => { 
-			return parseInt(n); 
-		}));
-	}
+  // TODO: validation
+  static _buf128_2_ipv6Str(buf) {
+    // It's an ipv4 mapped ipv6 address
+    if (buf.compare(Buffer.alloc(10), 0, 10, 0, 10) === 0 && 
+      buf.compare(Buffer.from([0xFF, 0xFF]), 0, 2, 10, 12) === 0) {
+      return `::FFFF:${Futil._buf32_2_ipv4Str(buf.slice(12, buf.length))}`;
+    }
 
-	// TODO: validation
-	static _buf32_2_ipv4Str(buf) {
-		return `${buf[0]}.${buf[1]}.${buf[2]}.${buf[3]}`;
-	}
+    let addr = "";
 
-	// TODO: Validation
-	static _ipv6Str2Buf128(str) {		
-		const arr = str.split(":");
-		const len = arr.length - 1;
+    for (let i = 0; i < buf.length; i += 2) {
+      addr += `${buf[i].toString(16).padStart(2, "0")}${buf[i + 1].toString(16).padStart(2, "0")}:`;
+    }
 
-		// It's an ipv4 mapped ipv6 address
-		if (net.isIPv4(arr[len]) && arr[len - 1].toUpperCase() === "FFFF") {
-			arr[len] = arr[len].split(".").map((n) => {
-				return parseInt(n).toString(16).padStart(2, "0");
-			}).join("");
-		}
-
-		const hs = arr.join("").padStart(16, "0");
-		const buf = Buffer.alloc(16);
-
-		let i = hs.length - 2;
-		let j = buf.length - 1;
-
-		while (i >= 0) {
-			buf[j] = parseInt(hs.substring(i, i + 2), 16);
-			i -= 2;
-			j -= 1;
-		}
-
-		return buf;
-	}
-
-	// TODO: validation
-	// Also this was very hastily written -- it needs unit tests
-	static _buf128_2_ipv6Str(buf) {
-		// It's an ipv4 mapped ipv6 address
-		if (buf.compare(Buffer.alloc(10), 0, 10, 0, 10) === 0 && buf.compare(Buffer.from([0xFF, 0xFF]), 0, 2, 10, 12) === 0) {
-			return `::FFFF:${Futil._buf32_2_ipv4Str(buf.slice(12, buf.length))}`;
-		}
-
-		let addr = "";
-
-		for (let i = 0; i < buf.length; i += 2) {
-			addr += `${buf[i].toString(16).padStart(2, "0")}${buf[i + 1].toString(16).padStart(2, "0")}:`;
-		}
-
-		return addr.substring(0, addr.length - 1); // Just remove the last colon
-	}
+    // Just remove the last colon
+    return addr.substring(0, addr.length - 1);
+  }
 }
 
 module.exports.Futil = Futil;
