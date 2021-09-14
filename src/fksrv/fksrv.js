5ff09b8b3265386c4df083cb390425fe3d428189
Noah Levenson <noahlevenson@gmail.com>
2021-09-14 10:54:50 -0400

    better comments for fksrv

--- src/fksrv/fksrv.js
+++ src/fksrv/fksrv.js
@@ -51,11 +51,20 @@ class Fksrv {
     t: 31337
   })
 
-  // TODO: write an Fdlt application layer base class, and Fksrv should subclass it
-  // The 3 hooks below, TX_VALID_HOOK, UTXO_DB_HOOK, UTXO_DB_INIT_HOOK, should be abstract methods
+  /**
+   * TODO: The intent of our design is to provide a very general distributed ledger which we can then
+   * build applications on top of. We did this because we know of at least two applications for the 
+   * distributed ledger: the keyserver, which you're looking at right now, and the decentralized 
+   * reputation/review system (coming soon). To better represent this architectural intent, there 
+   * should probably be a base class for an FDLT_APP, and FKSRV should subclass it. The 3 hooks below, 
+   * TX_VALID_HOOK, UTXO_DB_HOOK, UTXO_DB_INIT_HOOK, which are used to inject application-layer 
+   * functionality into FDLT processes, should be abstract methods of the Fdlt_app base class.
+   */
 
-  // TX_VALID_HOOK is where you specify any special validation logic for transactions beyond what's
-  // prescribed by the Fdlt layer. Should return true if valid, false if not.
+  /**
+   * TX_VALID_HOOK is where you specify any special validation logic for transactions beyond what's
+   * prescribed by the Fdlt layer. Should return true if valid, false if not.
+   */ 
   static TX_VALID_HOOK(tx_new, utxo_db) {
     // TODO: make sure that tx_new only has the scripts it's allowed to have
     
@@ -64,15 +73,19 @@ class Fksrv {
     return true;
   };
 
-  // UTXO_DB_HOOK is expected to modify a UTXO database as is required to accept a new valid 
-  // transaction, returning the new state of the database (the default behavior would be to set 
-  // tx_new under the key of its hash)
+  /**
+   * UTXO_DB_HOOK is expected to modify a UTXO database as is required to accept a new valid 
+   * transaction, returning the new state of the database (the default behavior would be to set 
+   * tx_new under the key of its hash)
+   */ 
   static UTXO_DB_HOOK(tx_new, utxo_db) {
     utxo_db.set(Fdlt_tsact.sha256(Fdlt_tsact.serialize(tx_new)), tx_new);
     return utxo_db;
   }
 
-  // UTXO_DB_INIT_HOOK is called when initializing a new UTXO DB before computing its state
+  /**
+   * UTXO_DB_INIT_HOOK is called when initializing a new UTXO DB before computing its state
+   */ 
   static UTXO_DB_INIT_HOOK(utxo_db) {
     // Set our persistent signature and revocation token
     utxo_db.set(Fksrv.SIG_TOK, Fksrv.SIG_TX);
@@ -98,10 +111,14 @@ class Fksrv {
     Flog.log(`[FKSRV] (${this.dlt.net.app_id}) Offline`);
   }
 
-  // TODO: We're currently preserving separate sign and revoke functions for compatability, but
-  // these functions can be collapsed into one parameterized function
+  /**
+   * TODO: We're currently preserving separate sign and revoke functions for compatability, but
+   * they can be collapsed into a single parameterized function
+   */ 
 
-  // Create a signing transaction: peer_a spends SIG_TOK on peer_b
+  /**
+   * Create a transaction for a key signing event: peer_a signs peer_b
+   */ 
   async sign(peer_a, peer_b) {
     const nonce = Array.from(Buffer.from(peer_a.nonce, "hex"));
     const peer_a_pubkey = Array.from(Buffer.from(peer_a.pubkey, "hex"));
@@ -120,10 +137,11 @@ class Fksrv {
       Fdlt_vm.make_sig_preimage(Fksrv.SIG_TX, tsact), Buffer.from(privkey, "hex")
     );
 
-    // Lock script: 
-    // push2, len, recip pubkey, push2, len, sig, push2, len, nonce, push2, len, my pubkey
-    // (push the recipient's pubkey to the stack just so we have it in the script)
-
+    /**
+     * Lock script: 
+     * push2, len, recip pubkey, push2, len, sig, push2, len, nonce, push2, len, my pubkey
+     * (push the recipient's pubkey to the stack just so we have it in the script)
+     */ 
     const lock_script = [].concat([
       Fdlt_vm.OPCODE.OP_PUSH2,
       ...Array.from(Futil.wbuf_uint16be(peer_b_pubkey.length)),
@@ -143,7 +161,9 @@ class Fksrv {
     return tsact;
   }
 
-  // Create a revocation transaction: peer_a spends REV_TOK on peer_b
+  /**
+   * Create a transaction for a key revocation event: peer_a revokes peer_b
+   */ 
   async revoke(peer_a, peer_b) {
     const nonce = Array.from(Buffer.from(peer_a.nonce, "hex"));
     const peer_a_pubkey = Array.from(Buffer.from(peer_a.pubkey, "hex"));
@@ -162,9 +182,11 @@ class Fksrv {
       Fdlt_vm.make_sig_preimage(Fksrv.REV_TX, tsact), Buffer.from(privkey, "hex")
     );
 
-    // Lock script: 
-    // push2, len, recip pubkey, push2, len, sig, push2, len, nonce, push2, len, my pubkey
-    // (push the recipient's pubkey to the stack just so we have it in the script)
+    /**
+     * Lock script: 
+     * push2, len, recip pubkey, push2, len, sig, push2, len, nonce, push2, len, my pubkey
+     * (push the recipient's pubkey to the stack just so we have it in the script)
+     */ 
     const lock_script = [].concat([
       Fdlt_vm.OPCODE.OP_PUSH2,
       ...Array.from(Futil.wbuf_uint16be(peer_b_pubkey.length)),
@@ -193,18 +215,26 @@ class Fksrv {
     this.dlt.broadcast(this.dlt.tx_req, {fdlt_tsact: tx});
   }
 
+  /** 
+   * Build the trust graph as computed over the current state of the distributed ledger. Returns
+   * an Fgraph
+   */ 
   build_wot() {
+    /**
+     * Helper function to extract the recipient's pubkey from a tx. The recpient's pubkey is the 
+     * bytes specified by the 0th OP_PUSH2 instruction (which happens to be the 0th byte of the script)
+     */ 
     function _get_recip_pubkey(tx) {
-      // The recpient's pubkey is the bytes specified by the 0th OP_PUSH2 instruction
-      // (which happens to be the 0th byte of the script)
       const recip_len = (tx.lock[1] << cfg.SYS_BYTE_WIDTH) | tx.lock[2];
       return tx.lock.slice(3, 3 + recip_len);
     }
 
+    /**
+     * Helper function to extract the sender's pubkey from a tx. The sender's pubkey is the bytes 
+     * specified by the 3rd OP_PUSH2 instruction (0-indexed). We find it by iteratively searching 
+     * over the script starting from the 0th byte, striding over each push operand
+     */ 
     function _get_sender_pubkey(tx) {
-      // If we 0-index the OP_PUSH2 instructions in the script, the signer's pubkey is the bytes 
-      // specified by the 3rd OP_PUSH2 instruction... let's find it by iteratively searching over 
-      // the script starting from the 0th byte, striding over each push operand
       let idx = 0;
       let start = 0;
       let count = 0;
@@ -219,8 +249,10 @@ class Fksrv {
       return tx.lock.slice(idx + 3, idx + 3 + signer_len);
     }
 
-    // If there's an unresolved accidental fork, we arbitrarily select the 0th branch
-    // TODO: this is almost definitely the wrong behavior
+    /**
+     * If there's an unresolved accidental fork, we arbitrarily select the 0th branch
+     * TODO: this is almost definitely the wrong behavior
+     */ 
     const db = this.dlt.build_db(this.dlt.store.get_deepest_blocks()[0]);
     const wot = new Fgraph();
 
