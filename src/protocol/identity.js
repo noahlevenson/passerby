435cb9388f13427f8ba17835085cd6fdd542c279
Noah Levenson <noahlevenson@gmail.com>
2022-02-03 20:10:13 -0500

    move pubkey and privkey wrappers to identity

--- src/protocol/identity.js
+++ src/protocol/identity.js
@@ -5,9 +5,35 @@ const Crypto = require("../core/crypto.js");
 class Identity {
   constructor() {
     const keypair = Crypto.crypto_box_keypair();
-    this.public_key = new Crypto.Public_key(keypair.publicKey, keypair.keyType);
-    this._private_key = new Crypto.Private_key(keypair.privateKey, keypair.keyType);
+    this.public_key = new Public_key(keypair.publicKey, keypair.keyType);
+    this._private_key = new Private_key(keypair.privateKey, keypair.keyType);
   }
 }
 
-module.exports = { Identity };
+class Public_key {
+  constructor(key, type) {
+    this.key = key;
+    this.type = type;
+  }
+
+  /**
+   * TODO: Validate and fail gracefully
+   */ 
+  static from_pubstring(pubstring) {
+    const sep = pubstring.indexOf(":");
+    return new this(Crypto.from_base64(pubstring.substring(0, sep)), pubstring.substring(sep + 1));
+  }
+
+  pubstring() {
+    return `${Crypto.to_base64(this.key)}:${this.type}`;
+  }
+}
+
+class Private_key {
+  constructor(key, type) {
+    this._key = key;
+    this.type = type;
+  }
+}
+
+module.exports = { Identity, Public_key, Private_key };
