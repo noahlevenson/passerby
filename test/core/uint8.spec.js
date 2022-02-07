6c2d855dd1776e329a4b1c9fd573e63946a6b954
Noah Levenson <noahlevenson@gmail.com>
2022-02-06 21:16:23 -0500

    add uint8 tests for 32-bit big endian

--- test/core/uint8.spec.js
+++ test/core/uint8.spec.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const assert = require("assert");
-const { compare, read_uint16le, write_uint16le } = require("../../src/core/uint8.js");
+const { compare, read_uint16le, write_uint16le, 
+  read_int32be, write_int32be } = require("../../src/core/uint8.js");
 
 /**
  * compare()
@@ -113,5 +114,96 @@ const { compare, read_uint16le, write_uint16le } = require("../../src/core/uint8
 
   assert.throws(() => {
     write_uint16le(31337, new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF]), 3)
+  });
+})();
+
+/**
+ * read_int32be()
+ */ 
+(() => {
+  assert.throws(() => {
+    read_int32be("food", 0);
+  });
+
+  assert.throws(() => {
+    read_int32be([0xDE, 0xAD, 0xBE, 0xEF], 0);
+  });
+
+  assert.throws(() => {
+    read_int32be(new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF]), 1);
+  });
+
+  assert.throws(() => {
+    read_int32be(new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF]), 2);
+  });
+
+  assert.throws(() => {
+    read_int32be(new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF]), 3);
+  });
+
+  assert.throws(() => {
+    read_int32be(new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF, 0xDE]), 2);
+  });
+
+  assert.doesNotThrow(() => {
+    read_int32be(new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF]), 0);
+  });
+
+  const deadbeefcafebabe = new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xFE, 0xBA, 0xBE]);
+
+  assert.strictEqual(read_int32be(deadbeefcafebabe, 0), -272716322);
+  assert.strictEqual(read_int32be(deadbeefcafebabe, 1), -890257747);
+  assert.strictEqual(read_int32be(deadbeefcafebabe, 2), -20254786);
+  assert.strictEqual(read_int32be(deadbeefcafebabe, 3), -1157707025);
+  assert.strictEqual(read_int32be(deadbeefcafebabe, 4), -1095041334);
+})();
+
+/**
+ * write_int32be()
+ */ 
+(() => {
+  assert.throws(() => {
+    write_int32be(3133731337, "foo bar", 0);
+  });
+
+  assert.throws(() => {
+    write_int32be(3133731337, [0xDE, 0xAD, 0xBE, 0xEF], 0);
+  });
+
+  assert.throws(() => {
+    write_int32be(3133731337, new Uint8Array(3), 0);
+  })
+
+  assert.throws(() => {
+    write_int32be(3133731337, new Uint8Array(16), 13);
+  });
+
+  assert.deepStrictEqual(
+    write_int32be(3133731337, new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xFE, 0xBA, 0xBE]), 0),
+    new Uint8Array([0x09, 0xF2, 0xC8, 0xBA, 0xCA, 0xFE, 0xBA, 0xBE])
+  );
+
+  assert.deepStrictEqual(
+    write_int32be(3133731337, new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xFE, 0xBA, 0xBE]), 1),
+    new Uint8Array([0xDE, 0x09, 0xF2, 0xC8, 0xBA, 0xFE, 0xBA, 0xBE])
+  );
+
+  assert.deepStrictEqual(
+    write_int32be(3133731337, new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xFE, 0xBA, 0xBE]), 2),
+    new Uint8Array([0xDE, 0xAD, 0x09, 0xF2, 0xC8, 0xBA, 0xBA, 0xBE])
+  );
+
+  assert.deepStrictEqual(
+    write_int32be(3133731337, new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xFE, 0xBA, 0xBE]), 3),
+    new Uint8Array([0xDE, 0xAD, 0xBE, 0x09, 0xF2, 0xC8, 0xBA, 0xBE])
+  );
+
+  assert.deepStrictEqual(
+    write_int32be(3133731337, new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xFE, 0xBA, 0xBE]), 4),
+    new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF, 0x09, 0xF2, 0xC8, 0xBA])
+  );
+
+  assert.throws(() => {
+    write_int32be(3133731337, new Uint8Array([0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xFE, 0xBA, 0xBE]), 5);
   });
 })();
