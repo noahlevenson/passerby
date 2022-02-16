fb1d60b4ba9b9c0f21d0ad80b99ad07cbdf5bc8c
Noah Levenson <noahlevenson@gmail.com>
2022-02-16 14:21:37 -0500

    local transport: intercept msgs with a callback instead of an event

--- src/transport/local/local.js
+++ src/transport/local/local.js
@@ -7,20 +7,20 @@ const { Transport, Rinfo } = require("../transport.js");
  * (e.g. a pubstring) to identify this peer in the network. my_port can be an arbitrary non-unique 
  * number. The first peer to be spawned in the emulated network should not be passed a peer_map; 
  * each subsequently spawned peer should be passed a reference to the first peer's peer_map. You 
- * should pass my_addr and my_port to Passerby.start() as described in the protocol documentation.
- * announcer is an optional EventEmitter; if one is supplied, this peer will announce its outbound
- * messages on it. By creating an emulated network of peers who all share the same announcer, you
- * can get a global view of the systemwide network traffic.
+ * should pass my_addr and my_port to Passerby.start as described in the protocol documentation.
+ * Callback send_cb(from, to, msg) is called each time a message is sent over the transport. Using
+ * send_cb, you can intercept each peer's outbound messages -- to provide, for example, a global 
+ * view of the systemwide network traffic.
  */ 
 
 class Local extends Transport {
-  constructor({my_addr, my_port = 31337, peer_map = new Map(), announcer = null} = {}) {
+  constructor({my_addr, my_port = 31337, peer_map = new Map(), send_cb = () => {}} = {}) {
     super();
     this.my_addr = my_addr;
     this.my_port = my_port;
     this.peer_map = peer_map;
     this.peer_map.set(this.my_addr, this);
-    this.announcer = announcer;
+    this.send_cb = send_cb;
   }
 
   /**
@@ -35,12 +35,8 @@ class Local extends Transport {
     }
 
     const my_rinfo = new Rinfo({address: this.my_addr, port: this.my_port, family: "local"});
-
-    if (this.announcer !== null) {
-      this.announcer.emit("message", this.my_addr, rinfo.address, msg);
-    }
-
     recip_transport.recv.emit("message", msg, my_rinfo);
+    this.send_cb(this.my_addr, rinfo.address, msg);
   }
 
   on_network() {
