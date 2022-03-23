31de08b05e379b11b56c5e2eac2d6bdb54828935
Noah Levenson <noahlevenson@gmail.com>
2022-03-23 15:27:48 -0400

    fix paths

--- build/install.sh
+++ build/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Execute this file from the passerby root directory, not /build!
+# Travis executes this file from the passerby root directory, not /build!
 
 git clone https://github.com/jedisct1/libsodium.js
 export NODE_PATH=./libsodium.js/dist/modules/
--- build/test.sh
+++ build/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Execute this file from the passerby root directory, not /build!
+# Travis executes this file from the passerby root directory, not /build!
 
 node ./test/core/math.spec.js
 node ./test/core/uint8.spec.js
