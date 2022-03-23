f38f23042aca2dfe3b4aa73b79928c589635842f
Noah Levenson <noahlevenson@gmail.com>
2022-03-23 15:37:51 -0400

    set travis env var

--- build/install.sh
+++ build/install.sh
@@ -3,5 +3,6 @@
 # Travis executes this file from the passerby root directory, not /build!
 
 git clone https://github.com/jedisct1/libsodium.js
-export NODE_PATH=/home/travis/build/noahlevenson/passerby/libsodium.js/dist/modules/
+# export NODE_PATH=/home/travis/build/noahlevenson/passerby/libsodium.js/dist/modules/
+# ^^^ This is configured in Travis settings
 cp ./build/passerby.json /home/travis/build/noahlevenson/passerby/
