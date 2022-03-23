ecc38620b264c3bca6b67c4f2478c9f10597b9da
Noah Levenson <noahlevenson@gmail.com>
2022-03-23 15:15:43 -0400

    make it work

--- build/test.sh
+++ build/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-node ./test/core/math.spec.js
-node ./test/core/uint8.spec.js
-node ./test/core/types/bigboy.spec.js
-node ./test/core/types/bintree.spec.js
+node ../test/core/math.spec.js
+node ../test/core/uint8.spec.js
+node ../test/core/types/bigboy.spec.js
+node ../test/core/types/bintree.spec.js
