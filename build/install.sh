9e783ab7fb58898cce94e609656264132b9d3398
Noah Levenson <noahlevenson@gmail.com>
2022-03-23 15:33:03 -0400

    set build dirs relative to passerby root

--- build/install.sh
+++ build/install.sh
@@ -3,5 +3,5 @@
 # Travis executes this file from the passerby root directory, not /build!
 
 git clone https://github.com/jedisct1/libsodium.js
-export NODE_PATH=./libsodium.js/dist/modules/
-cp ./build/passerby.json .
+export NODE_PATH=/home/travis/build/noahlevenson/passerby/libsodium.js/dist/modules/
+cp ./build/passerby.json /home/travis/build/noahlevenson/passerby/
