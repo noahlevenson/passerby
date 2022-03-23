bd44529f9aff92bdbdb9a9735a58183a5c229f03
Noah Levenson <noahlevenson@gmail.com>
2022-03-23 15:20:06 -0400

    one last build script fix

--- build/install.sh
+++ build/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Execute this file from the passerby root directory, not /build!
+
 git clone https://github.com/jedisct1/libsodium.js
 export NODE_PATH=./libsodium.js/dist/modules/
-cp ./build/passerby.json ..
+cp ./build/passerby.json .
--- build/test.sh
+++ build/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Execute this file from the passerby root directory, not /build!
+
 node ./test/core/math.spec.js
 node ./test/core/uint8.spec.js
 node ./test/core/types/bigboy.spec.js
