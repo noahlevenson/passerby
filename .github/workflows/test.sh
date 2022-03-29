d81336838a43b5b6c65a3345a1e57f6343dae8aa
Noah Levenson <noahlevenson@gmail.com>
2022-03-29 14:53:21 -0400

    fix test paths

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-node ~/passerby/test/core/math.spec.js
-node ~/passerby/test/core/uint8.spec.js
-node ~/passerby/test/core/types/bigboy.spec.js
-node ~/passerby/test/core/types/bintree.spec.js
+node $GITHUB_WORKSPACE/test/core/math.spec.js
+node $GITHUB_WORKSPACE/test/core/uint8.spec.js
+node $GITHUB_WORKSPACE/test/core/types/bigboy.spec.js
+node $GITHUB_WORKSPACE/test/core/types/bintree.spec.js
